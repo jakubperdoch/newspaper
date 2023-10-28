8053d5dbc760734104f2567d361ce5d7ed87b55f
Jakub Perdoch <perdochjakub@gmail.com>
2023-10-29 01:35:03 +0200

    + loading news from api

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -4,12 +4,14 @@ interface PostProps {
     title: string;
     description: string;
     urlToImage: string;
+    author: string;
   };
 }
 const Post = ({ item }: PostProps) => {
   const title = item.title;
   const description = item.description;
   const image = item.urlToImage;
+  const author = item.author;
 
   return (
     <>
@@ -17,6 +19,7 @@ const Post = ({ item }: PostProps) => {
         <span className="postTitle">{title}</span>
         <span className="postDescription">{description}</span>
         <img className="postImage" src={image} alt="" />
+        <span>by {author}</span>
       </div>
     </>
   );
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,54 +1,69 @@
 import "./_home.scss";
 import Post from "../../components/Post/Post";
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
+import axios from "axios";
 interface Article {
   title: string;
   description: string;
   urlToImage: string;
+  author: string;
 }
-const Home = () => {
-  const [items, setItems] = useState<Article[]>([]); // Specify the type as Article[]
-  const [page, setPage] = useState(1); // Track the current page
-  const [hasMore, setHasMore] = useState(true);
-  const [isLoaded, setLoaded] = useState(true);
 
-  const loadMoreData = () => {
-    const pageSize = 6; // Number of items to load in each request
-    const apiKey = "a95cef147016408992269caf44b4298a"; // Replace with your NewsAPI key
-    fetch(
-      `https://newsapi.org/v2/everything?q=apple&from=2023-10-21&to=2023-10-21&sortBy=popularity&pageSize=${pageSize}&page=${page}&apiKey=${apiKey}`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.articles.length === 0) {
-          // No more data available
+const Home = () => {
+  const [data, setData] = useState<Article[]>([]);
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(12);
+  const [hasMore, setHasMore] = useState(true);
+  const getNews = () => {
+    axios
+      .get(
+        `https://newsapi.org/v2/everything?q=art&page=${page}&pageSize=${pageSize}&apiKey=a95cef147016408992269caf44b4298a`
+      )
+      .then((response) => {
+        console.log(response);
+        const newArticles = response.data.articles;
+        if (newArticles.length === 0) {
+          // If there are no new articles, set hasMore to false
           setHasMore(false);
         } else {
-          setItems([...items, ...data.articles]);
+          setData([...data, ...newArticles]);
           setPage(page + 1);
         }
       });
   };
-
   useEffect(() => {
-    loadMoreData();
-  }, [isLoaded]);
+    getNews();
+  }, []);
+  const getMoreData = () => {
+    setTimeout(() => {
+      setPageSize(pageSize + 1);
+      setPage(page + 1);
+      getNews();
+    }, 1200);
+  };
 
   return (
     <div className="homePage">
       <span className="homeTitle">Latest Updates</span>
-      <InfiniteScroll
-        className="scroll"
-        dataLength={items.length}
-        next={loadMoreData}
-        hasMore={hasMore}
-        loader={<h4>Loading...</h4>}
-      >
-        {items.map((item, i) => (
-          <Post key={i} item={item} />
-        ))}
-      </InfiniteScroll>
+      <div className="scrollContainer">
+        <InfiniteScroll
+          className="scroll"
+          dataLength={data.length}
+          next={getMoreData}
+          hasMore={hasMore}
+          loader={<h5 className="loadingTitle">Loading...</h5>}
+          endMessage={
+            <p style={{ textAlign: "center" }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
+        >
+          {data.map((item, i) => (
+            <Post key={i} item={item} />
+          ))}
+        </InfiniteScroll>
+      </div>
     </div>
   );
 };
